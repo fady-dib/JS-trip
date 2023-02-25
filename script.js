1b83c6d87591e2140426a47c1ca31a3291df3172
Fadi <fadi@Fadis-MBP.lan>
2023-02-25 18:42:28 +0200

    declare JS variables

---
+++ script.js
@@ -0,0 +1,7 @@
+var first_name = document.getElementById('fname');
+var last_name = document.getElementById('lname');
+var email = document.getElementById('email');
+var year_of_birth = document.getElementById('yob');
+var password = document.getElementById('password');
+var repeat_password = document.getElementById('r-password');
+
